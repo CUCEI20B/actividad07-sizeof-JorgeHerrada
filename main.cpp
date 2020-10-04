36b100f3dc5d5bdb05151ca86d8a2769e8822a76
Jorge Herrada <jorge.herrada.28@gmail.com>
2020-10-04 15:34:15 -0500

    Agregamos if else para cada entrada de tipo de dato, funciona

--- main.cpp
+++ main.cpp
@@ -3,7 +3,33 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string tipoDeDato;
 
+    getline(cin,tipoDeDato);
+
+    if (tipoDeDato == "int")
+    {
+        cout<<sizeof(int)<<endl;
+    }
+    else if(tipoDeDato == "char")
+    {
+        cout<<sizeof(char)<<endl;
+    }
+    else if(tipoDeDato == "short")
+    {
+        cout<<sizeof(short)<<endl;
+    }
+    else if(tipoDeDato == "float")
+    {
+        cout<<sizeof(float)<<endl;
+    }
+    else if(tipoDeDato == "double")
+    {
+        cout<<sizeof(double)<<endl;
+    }
+    else if(tipoDeDato == "long")
+    {
+        cout<<sizeof(long)<<endl;
+    }
     return 0;
 }
